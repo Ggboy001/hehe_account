c415b628ad44958bc199a1143816eadf0f7f816e
Ggboy001 <yi1wonderful.gmail.com>
2023-12-19 19:25:27 +0800

    重构hehe-input组件，封装hehe-form组件

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,20 +2,15 @@
   <div class="header">
     <div class="left">
       <router-link to="/">
-        <i class="iconfont-hehe icon-logo">&#xe618;</i>禾禾账目</router-link
-      >
+        <i class="iconfont-hehe icon-logo">&#xe618;</i>禾禾账目</router-link>
     </div>
     <div class="middle">
       <div class="home"><router-link to="/">首页</router-link></div>
       <div class="account">
-        <router-link to="/account"
-          >账目<i class="iconfont-hehe icon-right">&#xe673;</i></router-link
-        >
+        <router-link to="/account">账目<i class="iconfont-hehe icon-right">&#xe673;</i></router-link>
       </div>
       <div class="ticket">
-        <router-link to="/ticket"
-          >票据<i class="iconfont-hehe icon-right">&#xe673;</i></router-link
-        >
+        <router-link to="/ticket">票据<i class="iconfont-hehe icon-right">&#xe673;</i></router-link>
       </div>
       <div class="post"><router-link to="/post">帖子</router-link></div>
     </div>
@@ -50,12 +45,15 @@
   color: #004751;
   margin-right: 5px;
 }
+
+
 .header {
   position: relative;
-  width: 100vw;
+  width: 100%;
   height: 60px;
   background-color: #e3eeff;
 }
+
 .header .left {
   font-size: 24px;
   line-height: 24px;
@@ -63,10 +61,12 @@
   left: 10%;
   top: 16px;
 }
+
 .header .middle {
   position: absolute;
   left: 40%;
   top: 20px;
+
   div {
     float: left;
     width: 6rem;
@@ -74,18 +74,22 @@
     font-size: 16px;
     height: 36px;
   }
+
   div:hover {
     border-bottom: 3px solid #000;
   }
 }
+
 .header .right {
   position: absolute;
   left: 70%;
   top: 20px;
+
   div {
     float: left;
     font-size: 16px;
   }
+
   .right-item1 {
     .personal {
       padding: 0 10px;
@@ -93,22 +97,26 @@
       border-left: #a69f9f solid 1px;
       border-right: #a69f9f solid 1px;
     }
+
     .icon-logout {
       font-size: 16px;
       color: #a69f9f;
     }
   }
 }
+
 .header .login-btn {
   position: absolute;
   right: 5%;
   top: 18px;
+
   .login {
     width: 50px;
     height: 30px;
     border: 0;
     background-color: #e3eeff;
   }
+
   .register {
     width: 50px;
     height: 30px;
--- src/components/common/hehe-input.vue
+++ src/components/common/hehe-input.vue
@@ -1,30 +1,65 @@
 <template>
     <div class="box">
         <div class="label">{{ placeholder }}</div>
-        <input :type="type" :value="value" @blur="onBlur" @input="onInput" :placeholder="placeholder" />
+        <div class="input-container">
+            <input :type="type" :value="value" @blur="onBlur" @input="onInput" :placeholder="placeholder" />
+            <span v-if="isCode" class="getCode">获取验证码</span>
+        </div>
         <div class="tip" v-if="!isValid && value !== ''">{{ tip }}</div>
         <div class="tip" v-else></div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 
+import { ref, onBeforeMount, onMounted } from 'vue';
+
+const placeholder = ref('')
+//输入框的值
+const value = ref('')
+//用于表示用户需要的输入框的类型，password,text
+const type = ref('')
+//手机号正则
 const phonePattern = /^1[34578]\d{9}$/
+//用于判定输入框内容是否有效，无则显示tip信息
 const isValid = ref(true)
+//用于判断输入框是否要有验证码
+const isCode = ref(false)
+//用于显示错误信息
 const tip = ref('')
-const emit = defineEmits(['update:value'])
+const emit = defineEmits(['update:value', 'getIsValid'])
+//根据use判断type，index用于区分不同的输入框，并与form组件进行通信
 const props = defineProps({
-    placeholder: String,
-    type: 'text' | 'password',
+    use: {
+        type: String,
+        default: ''
+    },
     value: String | Number,
-
+    index: Number,
 })
+onBeforeMount(() => {
+    //根据用处判断输入框类型和placeholder
+    if (props.use === 'phone') {
+        placeholder.value = '手机号'
+        type.value = 'text'
+    } else if (props.use === 'password') {
+        placeholder.value = '密码'
+        type.value = 'password'
+    } else if (props.use === 'code') {
+        placeholder.value = '验证码'
+        type.value = 'text'
+    }
+    if (placeholder.value === '验证码') {
+        isCode.value = true
+    }
+});
+
+//input组件输入时会触发该事件
 const onInput = (e) => {
     emit('update:value', e.target.value)
-
 }
 const onBlur = () => {
+
     //判断输入框是否为空,为空不进行表单提示,提高用户体验
     if (props.value === '') {
         isValid.value = true
@@ -32,29 +67,32 @@ const onBlur = () => {
         return
     }
     // 执行输入框的校验
-    if (props.placeholder === '手机号') {
+    if (props.use === 'phone') {
         isValid.value = phonePattern.test(props.value)
         tip.value = isValid.value ? '' : '手机号格式错误'
-    } else if (props.placeholder === '密码') {
+
+    } else if (props.use === 'password') {
         isValid.value = props.value.length >= 6
         tip.value = isValid.value ? '' : '密码长度至少为6位'
     }
+
+    emit('getIsValid', props.index, isValid.value)
+
 }
 </script>
 
 <style lang="less" scoped>
 .box {
-    margin-bottom: 16px;
+    height: 70px;
 
     .tip {
-
         color: #f56c6c;
         height: 12px;
     }
 }
 
 .label {
-    margin: 10px 0;
+
     font-size: 12px;
     color: #ccc;
 }
@@ -65,11 +103,25 @@ input {
     padding: 14px 20px;
     color: #5a5a5a;
     font-size: 16px;
-    width: 250px;
+    width: 260px;
     transition: all .2s;
 
 
 }
+
+.input-container {
+    width: 260px;
+    position: relative;
+
+    .getCode {
+        cursor: pointer;
+        position: absolute;
+        top: 15px;
+        right: 0;
+    }
+
+}
+
 
 input::placeholder {
     color: #ccc;
